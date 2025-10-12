20c32912d9ca8190ed4d03c7276e999134a6999e
Adam Griffin <ag@agriffin.me>
2025-10-12 16:02:56 -0700

    Added resume_session to home controller
    This is so it will authenticate the user if they are logged in by using the resume_session before_action but it will not require them to be logged in by using allow_unauthenticated_access.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,6 @@
 class HomeController < ApplicationController
+  # Authenticate the user if they're logged in with resume_session, but don't
+  # require them to be logged in to view the page
   allow_unauthenticated_access
+  before_action :resume_session
 end
