6c71cf36d22eed65805ffa66763a3356ee90301a
Adam Griffin <ag@agriffin.me>
2025-12-17 15:12:29 -0800

    Added tests for Review

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @review = reviews(:one)
+  end
+
+  test "should belong to user" do
+    assert_respond_to @review, :user
+  end
+
+  test "should belong to movie" do
+    assert_respond_to @review, :movie
+  end
+
+  test "should be valid with valid attributes" do
+    assert @review.valid?
+  end
+
+  test "should require user" do
+    @review.user = nil
+    assert_not @review.valid?
+  end
+
+  test "should require movie" do
+    @review.movie = nil
+    assert_not @review.valid?
+  end
+
+  test "should require body" do
+    @review.body = nil
+    assert_not @review.valid?
+  end
 end
