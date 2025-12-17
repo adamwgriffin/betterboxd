b141270489c332fc4b34aaffc885fd1752e95073
Adam Griffin <ag@agriffin.me>
2025-12-17 14:53:34 -0800

    Removed set_user
    Seems like it was pretty well customized for the show action so might as well just make it that.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,8 @@
 class UsersController < ApplicationController
   allow_unauthenticated_access
   before_action :resume_session
-  before_action :set_user, only: [ :show ]
 
   def show
-  end
-
-  private
-
-  def set_user
     @user = User.includes(:reviews).find_by!(username: params[:username])
     @reviews = @user.reviews.includes(:movie)
   rescue ActiveRecord::RecordNotFound
